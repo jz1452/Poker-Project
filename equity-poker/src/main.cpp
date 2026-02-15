603afcb7d1df89d731e1c9ac749f6e317f18c60e
Jordan <z5480808@ad.unsw.edu.au>
2026-02-15 21:02:38 +1100

    Cleaned up directory

--- equity-poker/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "poker/Card.h"
-#include "poker/Deck.h"
-#include "poker/Evaluator.h"
-#include <iostream>
-
-int main() {
-  std::cout << "Poker Evaluator Engine Ready." << std::endl;
-  return 0;
-}
